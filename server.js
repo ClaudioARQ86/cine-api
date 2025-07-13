84a6ba1d1cb5a717435a9fdce23fcd5d8f201fd8
ClaudioARQ86 <claudio.reyes.quiroga@outlook.es>
2025-07-12 23:38:51 -0400

    Puertos para render

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import client from './src/common/db.js'
 import peliculaRoutes from './src/pelicula/routes.js'
 import actorRoutes from './src/actor/routes.js'
 
-const PORT = 3000 || 4000
+const PORT = process.env.PORT || 3000 || 4000
 const app = express()
 
 app.use(express.json())
